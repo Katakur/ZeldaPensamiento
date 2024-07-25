6343946c90cc13f5f87d535e936e59fe008f0422
Ignacio Veas <Ignacio.veas.m@gmail.com>
2024-07-25 19:32:42 -0400

    Monster.dart
    actualizado

--- lib/model/monster.dart
+++ lib/model/monster.dart
@@ -1,25 +1,39 @@
 class Monster {
   String category;
-  String descripcion;
+  String description;
   int id;
   String image;
   String name;
+  List<String> commonLocations;
+  bool dlc;
+  List<String>? drops; // Nullable because drops can be null in the JSON
 
   Monster({
     required this.category,
-    required this.descripcion,
+    required this.description,
     required this.id,
     required this.image,
     required this.name,
+    required this.commonLocations,
+    required this.dlc,
+    this.drops,
   });
 
   factory Monster.fromJson(Map<String, dynamic> json) {
     return Monster(
       category: json['category'] as String,
-      descripcion: (json['descripcion'] as String?) ?? "",
+      description: json['description'] as String,
       id: json['id'] as int,
       image: json['image'] as String,
       name: json['name'] as String,
+      commonLocations: json['common_locations'] != null 
+          ? List<String>.from(json['common_locations'] as List<dynamic>)
+          : [], // Provide an empty list if null
+      dlc: json['dlc'] ?? false, // Provide a default value if null
+      drops: json['drops'] != null 
+          ? List<String>.from(json['drops'] as List<dynamic>) 
+          : null,
     );
   }
 }
+
