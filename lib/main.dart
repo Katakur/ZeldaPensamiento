77323ee767871d2a2663d0bb2f869fbcdf30a52d
Ignacio Veas <Ignacio.veas.m@gmail.com>
2024-07-25 18:18:35 -0400

    chekeo para que corra en android

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:ZeldaPensamiento/model/momo.dart';
+import 'package:zelda_pensamiento/model/momo.dart';
 //import 'package:proyecto_prueba/model/momo.dart';
 
 void main() {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ZeldaPensamiento/main.dart';
+import 'package:zelda_pensamiento/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
