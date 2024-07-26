9d9f44121d204cd00692ed2d44b1094274fba96f
Vaporu-Code <jpacevedob@gmail.com>
2024-07-26 17:36:00 -0400

    lista de favoritos
    se añadió una lista de favoritos en el home

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,16 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:zelda_pensamiento/treasure_page.dart';
-import 'package:zelda_pensamiento/monster_page.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:zelda_pensamiento/model/monster.dart';
+import 'package:zelda_pensamiento/model/equipment.dart';
+import 'package:zelda_pensamiento/model/treasure.dart';
+import 'package:zelda_pensamiento/treasure_display.dart';
+import 'package:zelda_pensamiento/monster_display.dart';
+import 'package:zelda_pensamiento/equipment_display.dart';
 import 'package:zelda_pensamiento/equipment_page.dart';
+import 'package:zelda_pensamiento/monster_page.dart';
+import 'package:zelda_pensamiento/treasure_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,6 +18,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Future<List<Treasure>> _treasures;
+  late Future<List<Monster>> _monsters;
+  late Future<List<Equipment>> _equipments;
+
+  @override
+  void initState() {
+    super.initState();
+    _treasures = _loadFavorites('treasurelist.json', (json) => Treasure.fromJson(json));
+    _monsters = _loadFavorites('monsterlist.json', (json) => Monster.fromJson(json));
+    _equipments = _loadFavorites('equipmentlist.json', (json) => Equipment.fromJson(json));
+  }
+
+  Future<File> _getLocalFile(String filename) async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File('${directory.path}/$filename');
+  }
+
+  Future<List<T>> _loadFavorites<T>(String filename, T Function(Map<String, dynamic>) fromJson) async {
+    try {
+      final file = await _getLocalFile(filename);
+      if (!file.existsSync()) {
+        throw Exception('File not found');
+      }
+      final jsonString = await file.readAsString();
+      final List<dynamic> jsonList = json.decode(jsonString)["data"];
+      return jsonList
+          .map((item) => fromJson(item as Map<String, dynamic>))
+          .where((item) => (item as dynamic).favorite) 
+          .toList();
+    } catch (e) {
+      print(e);
+      return [];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,11 +60,11 @@ class _HomePageState extends State<HomePage> {
         title: Text(
           'Home Page',
           style: TextStyle(
-            fontFamily: 'zelda', // Usa la fuente personalizada
-            fontSize: 20, // Ajusta el tamaño de la fuente según lo necesites
+            fontFamily: 'zelda',
+            fontSize: 20,
           ),
         ),
-        backgroundColor: Color.fromARGB(255, 205, 247, 253), // Cambia el color del AppBar
+        backgroundColor: Color.fromARGB(255, 205, 247, 253),
       ),
       drawer: Drawer(
         child: ListView(
@@ -30,9 +74,9 @@ class _HomePageState extends State<HomePage> {
               decoration: BoxDecoration(
                 gradient: LinearGradient(
                   colors: [
-                    Color.fromARGB(255, 146, 197, 130), 
+                    Color.fromARGB(255, 146, 197, 130),
                     Color.fromARGB(255, 80, 121, 76)
-                  ], // Degradado para el DrawerHeader
+                  ],
                 ),
               ),
               child: Text(
@@ -46,11 +90,11 @@ class _HomePageState extends State<HomePage> {
             ),
             ListTile(
               leading: Icon(Icons.star, color: Color.fromARGB(255, 118, 118, 118)),
-              title: const Text('Treasure', style: TextStyle(fontFamily: 'zelda')), 
+              title: const Text('Treasure', style: TextStyle(fontFamily: 'zelda')),
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => TreasurePage())
+                  MaterialPageRoute(builder: (context) => TreasurePage()),
                 );
               },
             ),
@@ -60,17 +104,17 @@ class _HomePageState extends State<HomePage> {
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => MonsterPage())
+                  MaterialPageRoute(builder: (context) => MonsterPage()),
                 );
               },
             ),
             ListTile(
               leading: Icon(Icons.shield, color: Color.fromARGB(255, 118, 118, 118)),
-              title: const Text('Equipment', style: TextStyle(fontFamily: 'zelda')), 
+              title: const Text('Equipment', style: TextStyle(fontFamily: 'zelda')),
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => EquipmentPage())
+                  MaterialPageRoute(builder: (context) => EquipmentPage()),
                 );
               },
             ),
@@ -79,20 +123,18 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Column(
         children: [
-          // Espacio para la descripción del juego
           Container(
-            color: Color.fromARGB(255, 80, 121, 76), // Asegúrate de que el fondo sea visible
+            color: Color.fromARGB(255, 80, 121, 76),
             padding: EdgeInsets.all(16.0),
             child: Text(
               'Aquí puedes poner una descripción del juego. Explica la historia, el objetivo, y cualquier detalle relevante que quieras compartir con el usuario.',
               style: TextStyle(
                 fontSize: 16,
                 color: Colors.white,
-                fontFamily: 'zelda', 
+                fontFamily: 'zelda',
               ),
             ),
           ),
-          // Espacio para la lista de acceso rápido a favoritos
           Expanded(
             child: Container(
               decoration: BoxDecoration(
@@ -100,7 +142,7 @@ class _HomePageState extends State<HomePage> {
                   colors: [
                     Color.fromARGB(255, 146, 197, 130),
                     Color.fromARGB(255, 80, 121, 76)
-                  ], // Degradado para el fondo de la lista
+                  ],
                   begin: Alignment.topLeft,
                   end: Alignment.bottomRight,
                 ),
@@ -108,27 +150,68 @@ class _HomePageState extends State<HomePage> {
               child: ListView(
                 padding: EdgeInsets.all(8.0),
                 children: [
-                  ListTile(
-                    leading: Icon(Icons.star, color: Color.fromARGB(255, 253, 255, 224)),
-                    title: Text('Favorite Item 1', style: TextStyle(fontFamily: 'zelda')),
-                    onTap: () {
-                      // Acción para el primer ítem favorito
-                    },
+                  Text('tesoros favoritos'),
+                  _buildFavoritesList(
+                    futureFavorites: _treasures,
+                    onTap: (id) => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => TreasureDisplay(id: id)),
+                    ),
                   ),
-                  ListTile(
-                    leading: Icon(Icons.star, color: Color.fromARGB(255, 253, 255, 224)),
-                    title: Text('Favorite Item 2', style: TextStyle(fontFamily: 'zelda')),
-                    onTap: () {
-                      // Acción para el segundo ítem favorito
-                    },
+                  Text('mounstruos favoritos'),
+                  _buildFavoritesList(
+                    futureFavorites: _monsters,
+                    onTap: (id) => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MonsterDisplay(id: id)),
+                    ),
                   ),
-                  // Agrega más ListTile según sea necesario
+                  Text('equipamiento favoritos'),
+                  _buildFavoritesList(
+                    futureFavorites: _equipments,
+                    onTap: (id) => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => EquipmentDisplay(id: id)),
+                    ),
+                  ),
                 ],
               ),
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildFavoritesList<T>({
+    required Future<List<T>> futureFavorites,
+    required void Function(int id) onTap,
+  }) {
+    return FutureBuilder<List<T>>(
+      future: futureFavorites,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text("Error: ${snapshot.error}"));
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return Center(child: Text("No hay ningún ítem favorito"));
+        } else {
+          final items = snapshot.data!;
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: items.map<Widget>((item) {
+              final id = (item as dynamic).id; 
+              final name = (item as dynamic).name; 
+              return ListTile(
+                leading: Icon(Icons.star, color: Color.fromARGB(255, 253, 255, 224)),
+                title: Text(name, style: TextStyle(fontFamily: 'zelda')),
+                onTap: () => onTap(id),
+              );
+            }).toList(),
+          );
+        }
+      },
     );
   }
 }
