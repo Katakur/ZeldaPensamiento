30fc5c0751875094ba476cd382506073a82b353e
Vaporu-Code <jpacevedob@gmail.com>
2024-07-25 20:23:48 -0400

    drawer del home
    se añadió un drawer en el home

---
+++ lib/home.dart
@@ -0,0 +1,75 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:zelda_pensamiento/tresure_page.dart';
+import 'package:zelda_pensamiento/monster_page.dart';
+import 'package:zelda_pensamiento/equipment_page.dart';
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+
+      drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(1, 1, 1, 1),
+                ),
+                child: Text('Menú'),
+              ),
+
+              ListTile(
+                title: const Text('Tesoros'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => TresurePage())
+                  );
+                },
+              ),
+
+              ListTile(
+                title: const Text('Mounstruos'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MonsterPage())
+                  );
+                },
+              ),
+
+              ListTile(
+                title: const Text('Equipo'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => EquipmentPage())
+                  );
+                },
+              ),
+
+            
+            ],
+          ),
+        ),
+
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:zelda_pensamiento/model/momo.dart';
-//import 'package:proyecto_prueba/model/momo.dart';
+import 'package:zelda_pensamiento/home.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-//cambio de prueba
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -21,73 +16,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late Future<List<Momo>> futureMomo;
-
-  @override
-  void initState() {
-    super.initState();
-    futureMomo = fetchMomo();
-  }
-
-  Future<List<Momo>> fetchMomo() async {
-    final response = await http.get(Uri.parse('https://botw-compendium.herokuapp.com/api/v3/compendium/category/monsters'));
-
-    if (response.statusCode == 200) {
-      final List<dynamic> jsonDecoded = jsonDecode(response.body)["data"] as List<dynamic>;
-      return jsonDecoded.map((dynamic item) => Momo.fromJson(item as Map<String, dynamic>)).toList();
-    } else {
-      throw Exception('Failed to load Momo');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: FutureBuilder<List<Momo>>(
-          future: futureMomo,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text("Error: ${snapshot.error}");
-            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return Text("No data available");
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  final momo = snapshot.data![index];
-                  return ListTile(
-                    leading: Image.network(momo.image, width: 50, height: 50, fit: BoxFit.cover),
-                    title: Text(momo.name),
-                    subtitle: Text("${momo.category}"),
-                  );
-                },
-              );
-            }
-          },
-        ),
-      ),
+      
+      home: HomePage(),
     );
   }
 }
